e2f26a47d228e523ad712ae5ececd945fc4fa018
Bùi Thanh Nam <80726325+anthonyBuiDev@users.noreply.github.com>
2024-03-18 17:59:24 +0700

    add style and supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import GlobalStyle from "./styles/GlobalStyles";
+import Dashboard from "./pages/Dashboard";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Login from "./pages/Login";
+import Bookings from "./pages/Bookings";
+import Account from "./pages/Account";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route
+              index
+              element={<Navigate replace to="dashboard" />}
+            />
+            <Route
+              path="dashboard"
+              element={<Dashboard />}
+            />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="account" element={<Account />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl =
+  "https://eeovkqiefwdzyqijwmyw.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlb3ZrcWllZndkenlxaWp3bXl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3NTYzNzEsImV4cCI6MjAyNjMzMjM3MX0.Xd37_xo9xRGMlMKi6vYLBtZ-IWTuSQMhJfppJDHbAAg";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
