7146c1b6ed4f0ed57c95f97b25e2a8ecf8f05e07
Bùi Thanh Nam <80726325+anthonyBuiDev@users.noreply.github.com>
2024-03-20 21:52:23 +0700

    add react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,42 @@
 import {
   BrowserRouter,
-  Routes,
-  Route,
   Navigate,
+  Route,
+  Routes,
 } from "react-router-dom";
-import GlobalStyle from "./styles/GlobalStyles";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import GlobalStyles from "./styles/GlobalStyles";
 import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
 import Cabins from "./pages/Cabins";
 import Users from "./pages/Users";
 import Settings from "./pages/Settings";
-import Login from "./pages/Login";
-import Bookings from "./pages/Bookings";
 import Account from "./pages/Account";
+import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+import { Toaster } from "react-hot-toast";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // staleTime: 60 * 1000,
+      staleTime: 0,
+    },
+  },
+});
 
 function App() {
   return (
-    <>
-      <GlobalStyle />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+
+      <GlobalStyles />
       <BrowserRouter>
         <Routes>
           <Route element={<AppLayout />}>
@@ -30,17 +48,39 @@ function App() {
               path="dashboard"
               element={<Dashboard />}
             />
+            <Route path="bookings" element={<Bookings />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="users" element={<Users />} />
-            <Route path="account" element={<Account />} />
-            <Route path="bookings" element={<Bookings />} />
             <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
           </Route>
+
           <Route path="login" element={<Login />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 5000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+          },
+        }}
+      />
+    </QueryClientProvider>
   );
 }
 
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl =
+export const supabaseUrl =
   "https://eeovkqiefwdzyqijwmyw.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVlb3ZrcWllZndkenlxaWp3bXl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA3NTYzNzEsImV4cCI6MjAyNjMzMjM3MX0.Xd37_xo9xRGMlMKi6vYLBtZ-IWTuSQMhJfppJDHbAAg";
